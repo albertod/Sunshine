1b9d0ac552c3a16bead5171d087b19725f0017e4
Alberto Di Martino <albertodmartino@gmail.com>
2016-09-22 00:57:41 -0700

    Now ForecastAdapter can handle 2 different type of layout for its views

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -6,46 +6,49 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * {@link ForecastAdapter} exposes a list of weather forecasts
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+
+    private final int VIEW_TYPE_TODAY = 0;
+    private final int VIEW_TYPE_FUTURE_DAY = 1;
+
+    private final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
 
-    /**
-     * Prepare the weather high/lows for presentation.
-     */
-    private String formatHighLows(double high, double low) {
-        boolean isMetric = Utility.isMetric(mContext);
-        String highLowStr = Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
-        return highLowStr;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT; // We have 2 types of view that can be used on the adapter
     }
 
     /*
-        This is ported from FetchWeatherTask --- but now we go straight from the cursor to the
-        string.
-     */
-    private String convertCursorRowToUXFormat(Cursor cursor) {
-        String highAndLow = formatHighLows(
-                cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP),
-                cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP));
-
-        return Utility.formatDate(cursor.getLong(ForecastFragment.COL_WEATHER_DATE)) +
-                " - " + cursor.getString(ForecastFragment.COL_WEATHER_DESC) +
-                " - " + highAndLow;
-    }
-
-    /*
-        Remember that these views are reused as needed.
-     */
+      Remember that these views are reused as needed.
+    */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-        return view;
+        int position = getItemViewType(cursor.getPosition());
+        int layoutId;
+
+        if (position == VIEW_TYPE_TODAY)
+            layoutId = R.layout.list_item_forecast_today;
+        else
+            layoutId = R.layout.list_item_forecast;
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
@@ -55,5 +58,25 @@ public class ForecastAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
+        String description = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+        TextView descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        descriptionView.setText(description);
+
+        boolean isMetric = Utility.isMetric(context);
+
+        double maxTemp = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        TextView maxTempView = (TextView)view.findViewById(R.id.list_item_high_textview);
+        maxTempView.setText(Utility.formatTemperature(maxTemp, isMetric));
+
+        double minTemp = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+        TextView minTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        minTempView.setText(Utility.formatTemperature(minTemp, isMetric));
+
+        long date = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+        TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        dateView.setText(Utility.getFriendlyDayString(context, date));
+
+        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        iconView.setImageResource(R.drawable.ic_launcher);
     }
 }
